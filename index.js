228b23d99233df18df7e9aa61db596acab572a76
fillerInk <thatonephysicsnerd@gmail.com>
2020-10-07 19:00:19 +0530

    Basic UI complete

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app = express()
+app.use(express.static(__dirname + '/views'));
+app.set('view engine', 'ejs');
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+app.get('/learn',(req,res)=>{
+    res.render('learn')
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+
+app.get('/editor',(req,res)=>{
+    res.render('editor')
+})
+
+const PORT = process.env.NODE_ENV || 5000
+
+app.listen(PORT,()=>{
+    console.log('Server has started')
+})
