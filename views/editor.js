dc66506e27f613021c61dfed017d02dd1ce5cd6e
fillerInk <thatonephysicsnerd@gmail.com>
2020-10-07 21:54:33 +0530

    Options menu mvp ready

--- views/editor.js
+++ views/editor.js
@@ -10,6 +10,40 @@ latex.innerHTML = '';
 document.head.appendChild(generator.stylesAndScripts("https://cdn.jsdelivr.net/npm/latex.js@0.12.4/dist/"))
 latex.appendChild(generator.domFragment())
 
+function insertAtCursor(myField, myValue) {
+    //IE support
+    if (document.selection) {
+        myField.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+    }
+    //MOZILLA and others
+    else if (myField.selectionStart || myField.selectionStart == '0') {
+        var startPos = myField.selectionStart;
+        var endPos = myField.selectionEnd;
+        myField.value = myField.value.substring(0, startPos)
+            + myValue
+            + myField.value.substring(endPos, myField.value.length);
+    } else {
+        myField.value += myValue;
+    }
+}
+
+function insert(command) {
+    console.log(command)
+    switch(command){
+        case 'section' : insertAtCursor(editor,'\\section{}')
+                         break
+        case 'listitem' : insertAtCursor(editor,'\\begin{itemize}\n    \\item\n\\end{itemize}')
+                          break;
+        case 'eqn' : insertAtCursor(editor,'\\[ x^n + y^n = z^n \\]')
+                          break;
+
+    }
+
+}
+
+
 function keystop(){
     var keystoppedTimer = null;
     var keystoppedInputs = document.getElementsByTagName('textarea');
